fecc971eb4f5193932377d13965c3fe7e804c828
Yogesh Ingulkar <yogeshingulkar750@gmail.com>
2025-08-08 17:24:40 +0530

    Add files via upload

---
+++ PlusOne.java
@@ -0,0 +1,28 @@
+class PlusOne{
+public static void main(String[] args) {
+
+
+    /**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+function plusOne(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            return digits;
+        } else {
+            digits[i] = 0;
+        }
+    }
+    
+    digits.unshift(1);
+    return digits;
+}
+
+console.log(plusOne([1, 2, 3]));
+console.log(plusOne([4, 3, 2, 1]));
+console.log(plusOne([9]));
+    
+}
+}
