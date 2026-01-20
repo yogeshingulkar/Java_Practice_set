926daf4146352902f69527e29032cff7d0f4a694
Yogesh Ingulkar <yogeshingulkar750@gmail.com>
2026-01-20 15:25:13 +0530

    Update 2ndLeastrepeatingElemement.java

--- 2ndLeastrepeatingElemement.java
+++ 2ndLeastrepeatingElemement.java
@@ -1,8 +1,6 @@
 import java.util.Arrays;
 class 2ndLeastrepeatingElemement{
-
 	public static void main(String[] args) {
-		
 		int [] arr = {3,1,5,6,4,5,4,5,3,2,2,6};
 		System.out.println(Arrays.toString(arr));
 		boolean [] fre = new boolean[arr.length];
@@ -10,6 +8,5 @@ class 2ndLeastrepeatingElemement{
 		int minRep = Integer.MAX_VALUE;
 		int minRep2 = Integer.MAX_VALUE;
 
-		
 	}
 }
