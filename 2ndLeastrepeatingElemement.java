ef2d3c0d430ec6291f24aff2ed94ae74ba09948e
Yogesh Ingulkar <yogeshingulkar750@gmail.com>
2026-02-12 22:26:28 +0530

    Update 2ndLeastrepeatingElemement.java

--- 2ndLeastrepeatingElemement.java
+++ 2ndLeastrepeatingElemement.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
+
 class 2ndLeastrepeatingElemement{
+
 	public static void main(String[] args) {
+	
 		int [] arr = {3,1,5,6,4,5,4,5,3,2,2,6};
 		System.out.println(Arrays.toString(arr));
 		boolean [] fre = new boolean[arr.length];
@@ -10,3 +13,4 @@ class 2ndLeastrepeatingElemement{
 
 	}
 }
+
